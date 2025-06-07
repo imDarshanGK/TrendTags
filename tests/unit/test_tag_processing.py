f0d88a7cc789dc9ce20ff9691be630c18ad970be
Dakota <73153176+dakotatokad@users.noreply.github.com>
2025-06-06 22:09:40 -0600

    tests: add tests for tag processing

---
+++ tests/unit/test_tag_processing.py
@@ -0,0 +1,22 @@
+from src import tag_processing
+import pytest
+
+@pytest.mark.parametrize(
+    "input_tags,expected_output",
+    [
+        (
+            ["#pythonLessons", "PythonPractice!", "#pythonsnakes"], 
+            ["pythonlessons", "pythonpractice", "pythonsnakes"]
+        ),
+        (["", "python", "java"], ["python"]),
+        (["123", "#@$pythonista", "!@#", ""], ["pythonista"]),
+        ([], []),
+        (
+            ["#Python_Rocks!", "@LearnPythonNow", "~python.dev~", "*Python*Powers*"],
+            ["python_rocks", "learnpythonnow", "python dev", "python powers"]
+        ),
+    ]
+)
+def test_keep_valid_tags(input_tags, expected_output):
+    topic = "python"
+    assert tag_processing.keep_valid_tags(input_tags, topic) == expected_output
