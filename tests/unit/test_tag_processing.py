351a8ecc239dfb15a1dc6874d0160938f3738bbc
Dakota <73153176+dakotatokad@users.noreply.github.com>
2025-06-06 22:09:14 -0600

    fix: relocate tag processing; avoid redefinition in loop
    Relocate the tag processing function to a new module.
    Modify how the function handles identifying the set of tags.
    Remove "magic" variables.
    
    chore: remove old code

--- tests/unit/test_tag_processing.py
+++ tests/unit/test_tag_processing.py
@@ -1,21 +1,23 @@
-from src import tag_processing
 import pytest
+
+from src import tag_processing
+
 
 @pytest.mark.parametrize(
     "input_tags,expected_output",
     [
         (
-            ["#pythonLessons", "PythonPractice!", "#pythonsnakes"], 
-            ["pythonlessons", "pythonpractice", "pythonsnakes"]
+            ["#pythonLessons", "PythonPractice!", "#pythonsnakes"],
+            ["pythonlessons", "pythonpractice", "pythonsnakes"],
         ),
         (["", "python", "java"], ["python"]),
         (["123", "#@$pythonista", "!@#", ""], ["pythonista"]),
         ([], []),
         (
             ["#Python_Rocks!", "@LearnPythonNow", "~python.dev~", "*Python*Powers*"],
-            ["python_rocks", "learnpythonnow", "python dev", "python powers"]
+            ["python_rocks", "learnpythonnow", "python dev", "python powers"],
         ),
-    ]
+    ],
 )
 def test_keep_valid_tags(input_tags, expected_output):
     topic = "python"
